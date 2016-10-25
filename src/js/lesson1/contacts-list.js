4d115103b8735b728c7df4ef2688ebc9f5bcf6de
Andrey Chechkin <andrey.chechkin@me.com>
2016-10-25 23:18:23 +0300

    Fix html

--- src/js/lesson1/contacts-list.js
+++ src/js/lesson1/contacts-list.js
@@ -2,33 +2,33 @@ import React from "react";
 
 const contactsList = [
 	{
-			id: 1,
-			name: 'Darth Vader',
-			phoneNumber: '+250966666666',
-			image: 'http://cs7.pikabu.ru/images/big_size_comm_an/2014-03_7/13962622876915.gif',
-			location: "Death Star",
-			age: 40
+		id: 1,
+		name: 'Darth Vader',
+		phoneNumber: '+250966666666',
+		image: 'http://cs7.pikabu.ru/images/big_size_comm_an/2014-03_7/13962622876915.gif',
+		location: "Death Star",
+		age: 40
 	}, {
-			id: 2,
-			name: 'Princess Leia',
-			phoneNumber: '+250966344466',
-			image: 'http://images6.fanpop.com/image/photos/33100000/CARRIE-FISHER-anakin-vader-and-princess-leia-33186069-190-149.gif',
-			location: "Ender",
-			age: 30
+		id: 2,
+		name: 'Princess Leia',
+		phoneNumber: '+250966344466',
+		image: 'http://images6.fanpop.com/image/photos/33100000/CARRIE-FISHER-anakin-vader-and-princess-leia-33186069-190-149.gif',
+		location: "Ender",
+		age: 30
 	}, {
-			id: 3,
-			name: 'Luke Skywalker',
-			phoneNumber: '+250976654433',
-			image: 'https://s-media-cache-ak0.pinimg.com/736x/61/cb/eb/61cbeb0572962f6ee6fc06d3f4576a64.jpg',
-			location: "Ender",
-			age: 25
+		id: 3,
+		name: 'Luke Skywalker',
+		phoneNumber: '+250976654433',
+		image: 'https://s-media-cache-ak0.pinimg.com/736x/61/cb/eb/61cbeb0572962f6ee6fc06d3f4576a64.jpg',
+		location: "Ender",
+		age: 25
 	}, {
-			id: 4,
-			name: 'Chewbacca',
-			phoneNumber: '+250456784935',
-			image: 'https://media.giphy.com/media/RUUdVZqwpfTRS/giphy.gif',
-			location: "Earth",
-			age: 100
+		id: 4,
+		name: 'Chewbacca',
+		phoneNumber: '+250456784935',
+		image: 'https://media.giphy.com/media/RUUdVZqwpfTRS/giphy.gif',
+		location: "Earth",
+		age: 100
 	}
 ];
 
