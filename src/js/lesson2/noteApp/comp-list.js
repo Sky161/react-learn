1fb2cd96b4c4bcfd7d58d0ef7c35f2ece002bf5e
Andrey Chechkin <andrey.chechkin@me.com>
2016-11-03 23:04:57 +0300

    Add masonry from note app

--- src/js/lesson2/noteApp/comp-list.js
+++ src/js/lesson2/noteApp/comp-list.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Masonry from "masonry-layout";
 import Note from "./comp-detail";
 
 export default class NoteList extends React.Component{
@@ -6,9 +7,20 @@ export default class NoteList extends React.Component{
 		super(props);
 	}
 
+	componentDidMount() {
+		console.log(this.refs);
+		const list = this.refs.list;
+		this.msnry = new Masonry(list, {
+			itemSelector: '.note',
+			columnWidth: 250,
+			gutter: 10,
+			isFitWidth: true
+		});
+	}
+
 	render() {
 		return(
-			<div className="list container-fluid">
+			<div className="list container-fluid" ref="list">
 				{
 					this.props.note.map((item, key) =>
 						<Note key={key} color={item.color}>
